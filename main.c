edc8c0d45893731f37846aaf4bcda30688e096ab
feihong.he <feihong.he@wz-inc.com>
2022-10-03 13:03:23 +0800

    Where it all started

---
+++ main.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: main.c
+    > Author: Sakura7301 
+    > Email: sakuraduck@foxmail.com 
+    > Github: https://github.com/Sakura7301 
+    > Created Time: 2022年10月03日 星期一 13时02分01秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<pthread.h>
+
